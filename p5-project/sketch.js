fa4df6b675f0b9882a6d57b080d8c721d77c0836
micaiahhill <118848571+micaiahhill@users.noreply.github.com>
2025-01-16 14:25:21 -0600

    Add files via upload

---
+++ p5-project/sketch.js
@@ -0,0 +1,14 @@
+
+function setup() {
+  createCanvas(190, 90);
+  
+}
+
+function draw() {
+  background(100,500,1);
+  circle(50,45,80);
+  square(110,10,70);
+}
+
+
+
---
+++ p5-project/sketch2.js
@@ -0,0 +1,17 @@
+
+function setup() {
+    createCanvas(200, 200);
+    
+  }
+  
+  function draw() {
+    background("blue");
+    circle(85,53,96);
+    circle(50,100,96);
+    circle(120,100,96);
+    
+  }
+  
+  
+  
+  
